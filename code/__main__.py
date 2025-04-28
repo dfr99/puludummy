948cf4d3b368bd0d62b24eca308a90802801dd5f
David Fraga Rodríguez <david.fraga@torusware.com>
2025-04-28 11:51:14 +0200

    Add Pulumi code

--- code/__main__.py
+++ code/__main__.py
@@ -4,10 +4,40 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('puludummy')
+bucket = s3.Bucket(
+    "puludummy",
+    website={
+        "index_document": "index.html",
+    },
+)
+
+ownership_controls = s3.BucketOwnershipControls(
+    "ownership-controls",
+    bucket=bucket.id,
+    rule={
+        "object_ownership": "ObjectWriter",
+    },
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    "public-access-block", bucket=bucket.id, block_public_acls=False
+)
+
+# Create an S3 Bucket object
+bucket_object = s3.BucketObject(
+    "index.html",
+    bucket=bucket.id,
+    source=pulumi.FileAsset("files/index.html"),
+    content_type="text/html",
+    acl="public-read",
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block, ownership_controls]),
+)
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+pulumi.export(
+    "bucket_endpoint", pulumi.Output.concat("http://", bucket.website_endpoint)
+)
 
 with open("./Pulumi.README.md") as f:
     pulumi.export("readme", f.read())
